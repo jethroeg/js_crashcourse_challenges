e5f0ca8ca4282bfc77df71a964a2bff43f6c81c1
jethroeg <troygatbunton@gmail.com>
2022-07-31 18:29:21 +0800

    init

---
+++ static/script.js
@@ -0,0 +1,24 @@
+// challenge 1: your age in days
+
+function ageInDays() {
+    var birthYear = prompt('What year were you born? ');
+    var ageInDayss = (2022-birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
+
+// challenge 2: cat generator
+
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
